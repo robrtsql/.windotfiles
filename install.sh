3e0d21a94b7505ec578d9c0c2f8d6d01942b783c
robrtsql <robrtsql@gmail.com>
2018-03-06 19:13:06 -0600

    Update install script to setup hyper config and plugins

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ cp -f c/Users/rob/AppData/Roaming/bug.n/Config.ini /c/Users/rob/AppData/Roaming/
 cp -f .vimrc ~
 cp -f .emacs ~
 cp -f .zshrc ~
+
+mkdir -p ~/.hyper_plugins
+npm install --prefix ~/.hyper_plugins hyper-snazzy
+cp -f .hyper.js ~
