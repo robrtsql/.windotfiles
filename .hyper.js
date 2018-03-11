a74523feb1fb3248b6e50486720e39ccb90934cf
robrtsql <robrtsql@gmail.com>
2018-03-11 13:11:20 -0500

    Update hyperjs config

--- .hyper.js
+++ .hyper.js
@@ -140,7 +140,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ['hyper-hover-header'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
